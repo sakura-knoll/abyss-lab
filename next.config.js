13d313ae870f139d1971402c22f75af2e240319e
Sakura Knoll <s4kura.knoll@gmail.com>
2022-02-25 09:55:14 +0900

    Update v5.5 data

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/honkai3rd/stigmata/bianka-stage',
+        destination: '/honkai3rd/stigmata/bianka-theatre',
+        permanent: true,
+      },
+      {
+        source: '/honkai3rd/stigmata/bianka-stage-top',
+        destination: '/honkai3rd/stigmata/bianka-theatre-top',
+        permanent: true,
+      },
+      {
+        source: '/honkai3rd/stigmata/bianka-stage-mid',
+        destination: '/honkai3rd/stigmata/bianka-theatre-mid',
+        permanent: true,
+      },
+      {
+        source: '/honkai3rd/stigmata/bianka-stage-bot',
+        destination: '/honkai3rd/stigmata/bianka-theatre-bot',
+        permanent: true,
+      },
+    ]
+  },
   i18n,
 }
