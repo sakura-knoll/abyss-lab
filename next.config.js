51b3e86d3361d9074f2671f6b79b8a89d1005509
Sakura Knoll <96382514+sakura-knoll@users.noreply.github.com>
2022-05-12 01:40:27 +0900

    Fix visual novels redirection (#98)

--- next.config.js
+++ next.config.js
@@ -64,38 +64,45 @@ module.exports = {
     return [
       {
         source: '/ko-KR/honkai3rd/novels/ae',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL + '/novels/ae/ko-KR/index.html',
+        destination: '/novels/ae/ko-KR/index.html',
+        locale: false,
+      },
+      {
+        source: '/en-US/honkai3rd/novels/ae',
+        destination: '/novels/ae/en-US/index.html',
         locale: false,
       },
       {
         source: '/honkai3rd/novels/ae',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL + '/novels/ae/en-US/index.html',
+        destination: '/novels/ae/en-US/index.html',
       },
       {
         source: '/ko-KR/honkai3rd/novels/duriduri',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL +
-          '/novels/duriduri/ko-KR/index.html',
+        destination: '/novels/duriduri/ko-KR/index.html',
+        locale: false,
+      },
+      {
+        source: '/en-US/honkai3rd/novels/duriduri',
+        destination: '/novels/duriduri/en-US/index.html',
         locale: false,
       },
       {
         source: '/honkai3rd/novels/duriduri',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL +
-          '/novels/duriduri/en-US/index.html',
+        destination: '/novels/duriduri/en-US/index.html',
       },
       {
         source: '/ko-KR/honkai3rd/novels/7s',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL + '/novels/7s/ko-KR/index.html',
+        destination: '/novels/7s/ko-KR/index.html',
+        locale: false,
+      },
+      {
+        source: '/en-US/honkai3rd/novels/7s',
+        destination: '/novels/7s/en-US/index.html',
         locale: false,
       },
       {
         source: '/honkai3rd/novels/7s',
-        destination:
-          process.env.NEXT_PUBLIC_BASE_URL + '/novels/7s/en-US/index.html',
+        destination: '/novels/7s/en-US/index.html',
       },
     ]
   },
