0544ef1615d9b32439e1c1886b10d9fc8650a6b5
Sakura Knoll <s4kura.knoll@gmail.com>
2022-07-21 09:16:32 +0900

    Add temp enemies page

---
+++ src/pages/honkai3rd/enemies.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { times } from 'ramda'
+
+interface BossSearchPageProps {}
+
+const AZArray = times((index) => {
+  const charCode = 'A'.charCodeAt(0) + index
+  const result = String.fromCharCode(charCode)
+  return result
+}, 26)
+const NumberArray = times((index) => {
+  if (index / 100 >= 1) {
+    return index
+  }
+  if (index / 10 >= 1) {
+    return '0' + index
+  }
+  return '00' + index
+}, 1000)
+
+const BossSearchPage = ({}: BossSearchPageProps) => {
+  return NumberArray.map((number) => number + '.png').map((item) => {
+    return (
+      <img
+        key={item}
+        src={`https://upload-os-bbs.mihoyo.com/game_record/honkai3rd/global/SpriteOutput/OpenWorld/QuestBossIcon/BOSS_${item}`}
+      />
+    )
+  })
+  // return AZArray.reduce<string[]>((array, firstChar) => {
+  //   return [
+  //     ...array,
+  //     ...AZArray.map((secondChar) => firstChar + secondChar + '_110.png'),
+  //   ]
+  // }, []).map((item) => {
+  //   return (
+  //     <img
+  //       key={item}
+  //       src={`https://upload-os-bbs.mihoyo.com/game_record/honkai3rd/global/SpriteOutput/OpenWorld/QuestBossIcon/${item}`}
+  //     />
+  //   )
+  // })
+}
+
+export default BossSearchPage
+
+// PSY : UL
+// MECHA : RO
+// BIO : DG
