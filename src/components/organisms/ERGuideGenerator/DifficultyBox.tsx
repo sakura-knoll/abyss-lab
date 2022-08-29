5aebcb05b9cad3cde9bf888d216c16d7e343ba5d
Sakura Knoll <s4kura.knoll@gmail.com>
2022-08-29 09:36:02 +0900

    Add shadow to difficulty box

--- src/components/organisms/ERGuideGenerator/DifficultyBox.tsx
+++ src/components/organisms/ERGuideGenerator/DifficultyBox.tsx
@@ -22,6 +22,7 @@ const DifficultyBox = ({ difficulty }: DifficultyBoxProps) => {
         padding: '5px',
         borderBottomLeftRadius: 10,
         borderBottomRightRadius: 10,
+        boxShadow: '5px 5px 10px rgba(0,0,0,0.5)',
       }}
       className='difficultyLabel'
     >
