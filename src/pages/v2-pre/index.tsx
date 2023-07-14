73aa3b5eb5bf884bf083a6aafed93e3b43189cbd
Sakura Knoll <s4kura.knoll@gmail.com>
2023-07-14 09:59:22 +0900

    Update v2 pre index page

--- src/pages/v2-pre/index.tsx
+++ src/pages/v2-pre/index.tsx
@@ -1,12 +1,28 @@
 import Link from 'next/link'
+import { Box } from 'theme-ui'
 
 const HomePage = () => {
   return (
     <div>
       <h1>V2 Pre</h1>
-      <Link href="/v2-pre/battlesuits">Battlesuits</Link>
-      <Link href="/v2-pre/weapons">Weapons</Link>
-      <Link href="/v2-pre/stigmata">Stigmata</Link>
+      <Box>
+        <Link href="/v2-pre/battlesuits">Battlesuits</Link>
+      </Box>
+      <Box>
+        <Link href="/v2-pre/weapons">Weapons</Link>
+      </Box>
+      <Box>
+        <Link href="/v2-pre/stigmata">Stigmata</Link>
+      </Box>
+      <Box>
+        <Link href="/v2-pre/stigmata-sets">StigmataSets</Link>
+      </Box>
+      <Box>
+        <Link href="/v2-pre/elfs">Elfs</Link>
+      </Box>
+      <Box>
+        <Link href="/v2-pre/er">Elysian Realm</Link>
+      </Box>
     </div>
   )
 }
